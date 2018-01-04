166ffc86447f592632394d41bed8b6824909de4f
Steph Bruyere <sjones11486@yahoo.com>
2018-01-03 23:53:50 -0500

    add admin pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
+import Admin from "./pages/admin";
 
 
 class App extends Component {
@@ -16,6 +17,8 @@ class App extends Component {
       <Switch>
       <Route exact path="/" component={Home} /> 
       <Route exact path="/contact" component={Contact} /> 
+      <Route exact path="/admin" component={Admin} /> 
+
       </Switch>    
       <Footer />
       </div>
