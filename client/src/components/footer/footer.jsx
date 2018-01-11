5573a354d45aa69bffdeaf223aa46f22bddb1c30
Steph Bruyere <sjones11486@yahoo.com>
2018-01-10 21:53:23 -0500

    updated footer

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./footer.css";
 
 const Footer = () =>
 <footer className="footer">
     <div className="container">
-        <span className="text-muted">Rccrafters Inc.</span><br/>
-        <span className="footerStyle">Home  |   Products   |   Contact us</span><br/>
-        <span className="footerStyle">RC CRAFTERS LOGOS, SITE DESIGN, & CONTENT © RC CRAFTERS  ALL RIGHTS RESERVED.</span>
-
+        <span className="text-muted">RC CRAFTER</span><br/>
+        <ul className="list-inline footerStyle">
+        <li className={window.location.pathname === "/" ? "active" : ""}>
+        <Link to="/contact">Home</Link></li>
+        <li className= {window.location.pathname === "/products" ? "active" : ""}>
+        <Link to="/contact">&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;|&nbsp;&nbsp;&nbsp; &nbsp;Products &nbsp;&nbsp;&nbsp; &nbsp;|&nbsp;&nbsp; &nbsp;&nbsp;</Link></li>     
+          <li className= {window.location.pathname === "/contact" ? "active" : ""}>
+          <Link to="/contact">Contact us</Link></li>
+        </ul>
+        <ul>
+        <li className="address">4646 Ashbury Park, North Olmsted, Ohio 44070</li> 
+        <li className="address">P. 440-777-4683 &nbsp; | &nbsp; C. 440-263-9807 &nbsp; | &nbsp; E. Rccrafters@aol.com</li>   
+        </ul>  
         </div>
 </footer>
 
